41d1b159ea80462d4bb4f1cd577d0c85c04ff32c
DanKos22 <G00397054@atu.ie>
2025-01-01 22:04:02 +0200

    Modified RabbitConfig class. Message queues are not working yet

--- src/main/java/ie/atu/userservice/PaymentNotificationListener.java
+++ src/main/java/ie/atu/userservice/PaymentNotificationListener.java
@@ -4,12 +4,23 @@ package ie.atu.userservice;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class PaymentNotificationListener {
-    @PostMapping("/payments-notifications")
+    private final List<String> receivedMessages = new ArrayList<>();
     @RabbitListener(queues = "paymentQueue")
     public void handlePaymentNotification(String message) {
+
         System.out.println(message);
+        //receivedMessages.add(message);
+        //return "message received";
+    }
+
+    public List<String> getReceivedMessages(@RequestBody Payment payment) {
+        return receivedMessages;
     }
 }
